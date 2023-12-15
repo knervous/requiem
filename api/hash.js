327eb53624024bc928e6bd237e37667169226ff2
Paul Johnson <Paul@PJOHNSOMAC-6366.local>
2023-12-15 15:42:40 -0500

    working

--- api/hash.js
+++ api/hash.js
@@ -1,9 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
  
+console.log('In module');
 export default function handler(
   request,
   response,
 ) {
+  console.log('in handler', request);
   response.status(200).json({
     test: 123
   });
